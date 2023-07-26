5646fdd56310b0007e27652643c69c7f97beef3a
Emmanuel BEMA <bema.emmanuel@yahoo.fr>
2023-07-26 19:28:25 +0100

    Test de mon premier commit

---
+++ src/main/java/com/adacorp/Test.java
@@ -0,0 +1,15 @@
+package com.adacorp;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test de creation de la methode");
+        // ceci est un commentaire sur une ligne
+        /**
+         * commentaire multilignes
+         *
+         */
+        System.out.println(" Bonjour "+ args[1]);
+        // ceci est un commentaire sur une ligne
+
+    }
+}
